b832ec12e345be517c9e35af2de1cabcdb5a6b02
Andrew Oberstar <ajoberstar@gmail.com>
2015-08-08 19:30:15 -0500

    Rough IFn to SAM for Java interop

--- src/ike/cljj/function.clj
+++ src/ike/cljj/function.clj
@@ -1,5 +1,37 @@
 (ns ike.cljj.function
-  (:import (java.util.function BiFunction Supplier Consumer Function)))
+  (:import (java.util.function BiFunction Supplier Consumer Function)
+           (java.lang.invoke MethodHandles MethodHandleProxies MethodType)))
+  
+(defn method-type [ret & parms]
+    (let [parm-array (into-array Class parms)]
+        (MethodType/methodType ret parm-array)))
+
+(defmulti method-handle (fn [kind receiver name type] kind))
+
+(defmethod method-handle :instance [kind receiver name type]
+    (.bind (MethodHandles/publicLookup) receiver name type))
+
+(defmethod method-handle :static [kind receiver name type]
+    (.findStatic (MethodHandles/publicLookup) receiver name type))
+
+(defmethod method-handle :virtual [kind receiver name type]
+    (.findVirtual (MethodHandles/publicLookup) receiver name type))
+
+(def array-type (class (into-array Object [])))
+
+(def seq-handle
+    (let [type (method-type clojure.lang.ISeq Object)]
+        (method-handle :static clojure.lang.RT "seq" type)))
+
+(def apply-handle
+    (let [type (method-type Object clojure.lang.ISeq)]
+        (method-handle :virtual clojure.lang.IFn "applyTo" type)))
+
+(defn invoke-handle [f]
+    (.asVarargsCollector (MethodHandles/collectArguments (.bindTo apply-handle f) 0 seq-handle) array-type))
+
+(defn sam-proxy [sam f]
+    (MethodHandleProxies/asInterfaceInstance sam (invoke-handle f)))
 
 (defn lambda [f]
   (reify
--- test/ike/cljj/function_test.clj
+++ test/ike/cljj/function_test.clj
@@ -1,1 +1,15 @@
-(ns ike.cljj.function-test)
+(ns ike.cljj.function-test
+	(:require [clojure.test :refer :all]
+		      [ike.cljj.function :refer :all]))
+
+(defn promising-fn [p]
+	(fn ([] (deliver p []))
+		([x] (deliver p [x]) x)
+		([x y] (deliver p [x y]) [x y])
+		([x y & zs] (deliver p [x y zs] [x y zs]))))
+
+(deftest supplier-works
+	(let [p (promise)
+		  f (promising-fn p)]
+		(is (= [] (.get (sam-proxy java.util.function.Supplier f))))
+		(is (= [] @p))))
