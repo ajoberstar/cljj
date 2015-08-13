96952049bc879ff75c797ecdd5a310b439a433a6
Andrew Oberstar <ajoberstar@gmail.com>
2015-08-12 20:41:40 -0500

    Add release tasks to lein.

--- project.clj
+++ project.clj
@@ -5,4 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.macro "0.1.2"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy" "clojars"]
+                  ["vcs" "push"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]}})
