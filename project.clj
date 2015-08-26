608e4429505529f1e8feddfcfc7d44ae8b2c3afd
Andrew Oberstar <ajoberstar@gmail.com>
2015-08-25 20:55:23 -0500

    Version 0.2.0-alpha4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ike/ike.cljj "0.2.0-alpha3"
+(defproject ike/ike.cljj "0.2.0-alpha4"
   :description "Clojure to Java interop APIs"
   :url "https://github.com/ike-tools/ike.cljj"
   :license {:name "Eclipse Public License"
