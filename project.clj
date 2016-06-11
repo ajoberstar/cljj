2d2f0cdc8958db0176e58f45f5e4265664411b61
Andrew Oberstar <ajoberstar@gmail.com>
2016-06-11 09:22:19 -0500

    Deprecating before move to ajoberstar namespace

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ike/ike.cljj "0.3.0-dev.1"
-  :description "Clojure to Java interop APIs"
-  :url "https://github.com/ike-tools/ike.cljj"
+(defproject ike/ike.cljj "0.2.3"
+  :description "DEPRECATED: Use org.ajoberstar/clj-java"
+  :url "https://github.com/ajoberstar/clj-java"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
