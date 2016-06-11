723f8e0419d49a64afca2b74eca2123490dae474
Andrew Oberstar <ajoberstar@gmail.com>
2016-06-11 09:29:57 -0500

    temporarily disable signing releases

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.macro "0.1.2"]]
+  :repositories [["clojars" {:url "http://clojars.org/repo"
+                             :sign-releases false}]]
   :release-tasks [["vcs" "assert-committed"]
                   ["vcs" "tag"]
                   ["deploy" "clojars"]
