74df452b58476fa90f76f4651cb0558df7caa326
Andrew Oberstar <ajoberstar@gmail.com>
2017-05-29 09:31:40 -0500

    bump deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ajoberstar/ike.cljj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/tools.macro "0.1.2"]]
   :release-tasks [["vcs" "assert-committed"]
                   ["vcs" "tag"]
