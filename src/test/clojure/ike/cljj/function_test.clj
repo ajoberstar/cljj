f39c7ea6fda1131b3facd9f9971d34082184d0f8
Andrew Oberstar <ajoberstar@gmail.com>
2018-05-05 22:00:25 -0500

    Type hints in function test
    The function namespace requires type hints in many cases to avoid
    reflection which fails in Java 9+.

--- src/test/clojure/ike/cljj/function_test.clj
+++ src/test/clojure/ike/cljj/function_test.clj
@@ -4,33 +4,32 @@
   (:import (java.util.function Supplier Consumer Function BiFunction)))
 
 (deftest supplier
-  (let [f (sam* Supplier (fn [] "supplied"))]
+  (let [^Supplier f (sam* Supplier (fn [] "supplied"))]
     (is (= "supplied" (.get f)))))
 
 (deftest consumer
   (let [p (promise)
-        f (sam* Consumer (fn [x] (deliver p x)))]
+        ^Consumer f (sam* Consumer (fn [x] (deliver p x)))]
     (is (nil? (.accept f 123)))
     (is (= 123 @p))))
 
 (deftest function
-  (let [f (sam* Function (fn [x] (* x 2)))]
+  (let [^Function f (sam* Function (fn [x] (* x 2)))]
     (is (= 4 (.apply f 2)))))
 
 (deftest bifunction
-  (let [f (sam* BiFunction (fn [x y] (* x y)))]
+  (let [^BiFunction f (sam* BiFunction (fn [x y] (* x y)))]
     (is (= 6 (.apply f 2 3)))))
 
-
 (deftest sam-macro-supplier
-  (let [f (sam Supplier [] "supplied")]
+  (let [^Supplier f (sam Supplier [] "supplied")]
     (is (= "supplied" (.get f)))))
 
 (deftest defsam-macro-supplier
-  (defsam f Supplier [] "supplied")
-  (is (= "supplied" (.get f))))
+  (defsam f1 Supplier [] "supplied")
+  (is (= "supplied" (.get ^Supplier f1))))
 
 (deftest defsam-docstring-macro-supplier
-  (defsam f "It worked!" Supplier [] "supplied")
-  (is (= "supplied" (.get f)))
-  (is (= "It worked!" (:doc (meta #'f)))))
+  (defsam f2 "It worked!" Supplier [] "supplied")
+  (is (= "supplied" (.get ^Supplier f2)))
+  (is (= "It worked!" (:doc (meta #'f2)))))
