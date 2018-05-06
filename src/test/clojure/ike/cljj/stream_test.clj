36b159364bef91d0f5e34a263cb0ce489a1ddcd3
Andrew Oberstar <ajoberstar@gmail.com>
2018-05-06 08:35:02 -0500

    Change stream test to work on Java 9
    Clojure is generating the wrong bytecode on Java 9+ for static methods
    on interfaces. Since, IntStream is an interface the IntStream/range
    calls were failing due to this. Switched to an alternante way to get a
    stream so the tests will work.
    
    See Clojure bug: https://dev.clojure.org/jira/browse/CLJ-2284

--- src/test/clojure/ike/cljj/stream_test.clj
+++ src/test/clojure/ike/cljj/stream_test.clj
@@ -2,34 +2,39 @@
   (:require [clojure.test :refer :all]
             [ike.cljj.stream :refer :all]
             [ike.cljj.function :refer [sam*]])
-  (:import (java.util.stream IntStream)
-           (java.util.function IntPredicate IntUnaryOperator)))
+  (:import [java.util ArrayList]
+           [java.util.function Predicate UnaryOperator]))
+
+(defn range-stream [from to]
+  (-> (range from to)
+      (ArrayList.)
+      (.stream)))
 
 (deftest stream-reduce
-  (let [stream (IntStream/range 0 10)]
+  (let [stream (range-stream 0 10)]
     (is (= 45 (reduce + stream)))))
 
 (deftest stream-into
-  (let [stream (IntStream/range 0 5)]
+  (let [stream (range-stream 0 5)]
     (is (= [0 1 2 3 4] (into [] stream)))))
 
 (deftest stream-transduce
-  (let [stream (IntStream/range 0 10)]
+  (let [stream (range-stream 0 10)]
     (is (= 25 (transduce (filter odd?) + stream)))))
 
 (deftest stream-sequence
-  (let [stream (IntStream/range 0 10)
+  (let [stream (range-stream 0 10)
         sseq (sequence (filter odd?) (stream-seq stream))]
     (is (seq? sseq))
     (is (= '(1 3 5 7 9) sseq))))
 
 (deftest stream-eduction
-  (let [stream (IntStream/range 0 10)
+  (let [stream (range-stream 0 10)
         educ (eduction (filter odd?) (stream-seq stream))]
     (is (= 25 (reduce + 0 educ)))))
 
 (deftest stream-prefiltered-into
-  (let [stream (-> (IntStream/range 0 10)
-                   (.filter (sam* IntPredicate even?))
-                   (.map (sam* IntUnaryOperator (fn [x] (int (* x 3))))))]
+  (let [stream (-> (range-stream 0 10)
+                   (.filter (sam* Predicate even?))
+                   (.map (sam* UnaryOperator (fn [x] (int (* x 3))))))]
     (is (= [0 6 12 18 24] (into [] stream)))))
